d3ef454c1162ab406be1f43ccd5cfeab63a7961b
unknown <mariscafe@gmail.com>
2021-03-03 18:00:34 -0300

    Início do Projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace videos_catalog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
